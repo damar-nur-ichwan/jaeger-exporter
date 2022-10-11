854f201afcc4f1026fe75e082384b1fdc8d14b65
damar-nur-ichwan <damarnurichwan.official@gmail.com>
2022-10-12 01:44:21 +0700

    set to denied span that has "undefined" value

--- utils/BuildConnectivityData/BuildConnectivityData.util.js
+++ utils/BuildConnectivityData/BuildConnectivityData.util.js
@@ -51,94 +51,112 @@ function buildConnectivityData({
 
 function createKey(keys = []) {
   return keys
-    .filter((value, index, self) => self.indexOf(value) === index)
+    .filter((value, index, self) => value && self.indexOf(value) === index)
     .map((value) => `<${value}>`)
     .reduce((a, b) => (a += "," + b));
 }
 
 function coupleSpanConnectivity(coupleSpans = []) {
-  return coupleSpans.map(({ clientSpan, serviceSpan }) => {
-    const client = clientSpan.tags["service.name"];
-    const clientGroup = clientSpan.tags["service.group"]
-      ? clientSpan.tags["service.group"]
-      : serviceSpan.tags["service.group"];
-    const service = serviceSpan.tags["db.name"]
-      ? "db." + serviceSpan.tags["db.name"]
-      : serviceSpan.tags["service.name"]
-      ? serviceSpan.tags["service.name"]
-      : "peer." + clientSpan.tags["peer.service"];
-    const serviceGroup = serviceSpan.tags["service.group"]
-      ? serviceSpan.tags["service.group"]
-      : clientSpan.tags["service.group"];
-    const key = createKey([client, service, clientGroup, serviceGroup]);
-    const responseTime = parseInt(serviceSpan["duration"] / 1000);
-    const startTime = clientSpan.startTime;
-    const status = serviceSpan.tags["otel.status_code"] || "OK";
-    return {
-      startTime,
-      client,
-      service,
-      clientGroup,
-      serviceGroup,
-      key,
-      status,
-      responseTime,
-    };
-  });
+  return coupleSpans
+    .map(({ clientSpan, serviceSpan }) => {
+      const client = clientSpan.tags["service.name"];
+      const clientGroup = clientSpan.tags["service.group"]
+        ? clientSpan.tags["service.group"]
+        : serviceSpan.tags["service.group"];
+      const service = serviceSpan.tags["db.name"]
+        ? "db." + serviceSpan.tags["db.name"]
+        : serviceSpan.tags["service.name"]
+        ? serviceSpan.tags["service.name"]
+        : "peer." + clientSpan.tags["peer.service"];
+      const serviceGroup = serviceSpan.tags["service.group"]
+        ? serviceSpan.tags["service.group"]
+        : clientSpan.tags["service.group"];
+      const key = createKey([client, service, clientGroup, serviceGroup]);
+      const responseTime = parseInt(serviceSpan["duration"] / 1000);
+      const startTime = clientSpan.startTime;
+      const status = serviceSpan.tags["otel.status_code"] || "OK";
+      return {
+        startTime,
+        client,
+        service,
+        clientGroup,
+        serviceGroup,
+        key,
+        status,
+        responseTime,
+      };
+    })
+    .filter(
+      (value, index, self) =>
+        !JSON.stringify(value).includes("undefined") &&
+        self.indexOf(value) === index
+    );
 }
 
 function singleClientSpansConnectivity(singleClientSpans = []) {
-  return singleClientSpans.map((singleClientSpan) => {
-    const client = singleClientSpan.tags["service.name"];
-    const clientGroup = singleClientSpan.tags["service.group"];
-    const service = singleClientSpan.tags["db.name"]
-      ? "db." + singleClientSpan.tags["db.name"]
-      : singleClientSpan.tags["http.host"]
-      ? "http." + singleClientSpan.tags["http.host"]
-      : "peer." + singleClientSpan.tags["peer.service"];
-    const key = createKey([client, service, clientGroup]);
-    const responseTime = parseInt(singleClientSpan["duration"] / 1000);
-    const startTime = singleClientSpan.startTime;
-    const status = singleClientSpan.tags["otel.status_code"] || "OK";
-    return {
-      startTime,
-      client,
-      service,
-      clientGroup,
-      key,
-      status,
-      responseTime,
-    };
-  });
+  return singleClientSpans
+    .map((singleClientSpan) => {
+      const client = singleClientSpan.tags["service.name"];
+      const clientGroup = singleClientSpan.tags["service.group"];
+      const service = singleClientSpan.tags["db.name"]
+        ? "db." + singleClientSpan.tags["db.name"]
+        : singleClientSpan.tags["http.host"]
+        ? "http." + singleClientSpan.tags["http.host"]
+        : "peer." + singleClientSpan.tags["peer.service"];
+      const key = createKey([client, service, clientGroup]);
+      const responseTime = parseInt(singleClientSpan["duration"] / 1000);
+      const startTime = singleClientSpan.startTime;
+      const status = singleClientSpan.tags["otel.status_code"] || "OK";
+      return {
+        startTime,
+        client,
+        service,
+        clientGroup,
+        key,
+        status,
+        responseTime,
+      };
+    })
+    .filter(
+      (value, index, self) =>
+        !JSON.stringify(value).includes("undefined") &&
+        self.indexOf(value) === index
+    );
 }
 
 function singleServiceSpansConnectivity(singleServiceSpans = []) {
-  return singleServiceSpans.map((singleServiceSpan) => {
-    const service = singleServiceSpan.tags["service.name"];
-    const serviceGroup = singleServiceSpan.tags["service.group"];
-    const userAgent =
-      singleServiceSpan.tags["http.user_agent"] &&
-      !singleServiceSpan.tags["http.user_agent"].includes(" ") &&
-      singleServiceSpan.tags["http.user_agent"].includes(".")
-        ? singleServiceSpan.tags["http.user_agent"]
-        : false;
-    const client = userAgent ? userAgent.split(".")[1] : "Client";
-    const clientGroup = userAgent ? userAgent.split(".")[0] : false;
+  return singleServiceSpans
+    .map((singleServiceSpan) => {
+      const service = singleServiceSpan.tags["service.name"];
+      const serviceGroup = singleServiceSpan.tags["service.group"];
+      const userAgent =
+        singleServiceSpan.tags["http.user_agent"] &&
+        !singleServiceSpan.tags["http.user_agent"].includes(" ") &&
+        singleServiceSpan.tags["http.user_agent"].includes(".")
+          ? singleServiceSpan.tags["http.user_agent"]
+          : false;
+      const client = userAgent ? userAgent.split(".")[1] : "Client";
+      const clientGroup = userAgent ? userAgent.split(".")[0] : false;
 
-    const key = clientGroup
-      ? createKey([client, service, serviceGroup, clientGroup])
-      : createKey([client, service, serviceGroup]);
-    const responseTime = parseInt(singleServiceSpan["duration"] / 1000);
-    const startTime = singleServiceSpan.startTime;
-    const status = singleServiceSpan.tags["otel.status_code"] || "OK";
-    return {
-      startTime,
-      client,
-      service,
-      serviceGroup,
-      key,
-      status,
-      responseTime,
-    };
-  });
+      const key = clientGroup
+        ? createKey([client, service, serviceGroup, clientGroup])
+        : createKey([client, service, serviceGroup]);
+      const responseTime = parseInt(singleServiceSpan["duration"] / 1000);
+      const startTime = singleServiceSpan.startTime;
+      const status = singleServiceSpan.tags["otel.status_code"] || "OK";
+      return {
+        startTime,
+        client,
+        service,
+        serviceGroup,
+        key,
+        status,
+        responseTime,
+      };
+    })
+    .filter(
+      (value, index, self) =>
+        !JSON.stringify(value).includes("undefined") &&
+        self.indexOf(value) === index
+    );
 }
